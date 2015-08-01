8168760d245cb2a9c3ad330c6038999cbdfee963
porten <porten@gmail.com>
2015-08-01 08:17:04 +0200

    v3.1.1

--- GuildBankTools.lua
+++ GuildBankTools.lua
@@ -10,7 +10,7 @@ require "Window"
 	--[[ Globals and enums --]]
 
 -- Addon class itself
-local Major, Minor, Patch = 3, 1, 0
+local Major, Minor, Patch = 3, 1, 1
 local GuildBankTools = {}
 
 -- Ref to the GuildBank addon
